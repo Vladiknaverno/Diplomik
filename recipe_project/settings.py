fcff22b9bc160260d981b0f53714244bf239236c
vshad <v.shad2003@gmail.com>
2025-05-05 16:39:43 +0300

    ласт апдейт

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'recipes/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
